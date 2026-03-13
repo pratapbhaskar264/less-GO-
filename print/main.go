e8a6d755904892e0f810a20520ffd1923f4bcc87
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-13 18:38:03 +0530

    DAY-3-Println/Printf

--- print/main.go
+++ print/main.go
@@ -6,6 +6,10 @@ func main(){
 	name := "alice"
 	marks := 67
 
-	fmt.Println("name :" , name , " , marks: " , marks) // changes line and add spaces req
-	// fmt.Printf("name :" , name , " , marks: " , marks)
+	// fmt.Println("name :" , name , " , marks: " , marks) // changes line and add spaces req
+	// fmt.Printf("name :" , name , " , marks: \n" , marks) // works with format specifiers 
+	// fmt.Printf("Name is : %s\n " , name)
+	// fmt.Printf("Type of name is %T\n" , name) // type check %T
+
+	fmt.Printf( "Name is : %s\nMarks are : %d " , name , marks ) // multiple in same line 
 }
