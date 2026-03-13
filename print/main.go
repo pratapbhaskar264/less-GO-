30fcb389d7ac887d324f0b92bf7230927bc88aba
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-13 18:23:22 +0530

    DAY-3-Println/Printf

---
+++ print/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" 
+
+func main(){
+	name := "alice"
+	marks := 67
+
+	fmt.Println("name :" , name , " , marks: " , marks) // changes line and add spaces req
+	// fmt.Printf("name :" , name , " , marks: " , marks)
+}
