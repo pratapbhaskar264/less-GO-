533e73f0d7b0ccb5f7dc1450b570210f4776d87c
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-13 17:58:43 +0530

    DAY-2-Variables

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-	"lessGo/utils"
+	// "lessGo/utils"
 )
 
 func main() {
 	fmt.Println("Hello mam")
-	utils.Greet("Good evening 23 times ")
+	// utils.Greet("Good evening 23 times ")
+	var name string = "Hey ! how are you"
+	fmt.Println(name)
+
+	var float float64 = 264.45
+	fmt.Println(float)
+	
+	var float2  = 264.45 //type inference
+	fmt.Println(float2)
 }
