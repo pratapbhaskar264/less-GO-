4becf7620173da7b951517b480cb6a6cccf73262
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-13 18:07:07 +0530

    DAY-2

--- main.go
+++ main.go
@@ -16,4 +16,13 @@ func main() {
 	
 	var float2  = 264.45 //type inference
 	fmt.Println(float2)
+
+	const truth = "rohit shrama is GOAT" //never changing value
+	fmt.Println(truth)
+	name = "ok"
+	fmt.Println(name)
+
+
+	other_trick := "hehehaha"
+	fmt.Println(other_trick) //wow 
 }
