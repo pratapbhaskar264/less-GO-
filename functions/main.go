ab6204bc9a2bc28f2cfaa676d57bcb3e22ecc905
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-14 12:51:45 +0530

    DAY-5-functions

--- functions/main.go
+++ functions/main.go
@@ -6,7 +6,15 @@ func wish() { // simple wish program
 	fmt.Println("hey")
 }
 
+func add(a int , b int) (int) { //note the return type format
+	return a+b
+}
+
 func main() {
 	fmt.Println("hey Fuction learning")
 	wish()
+	// var a int = 3
+	// var b int = 4
+	// fmt.Println(add(a,b))
+	fmt.Println(add(2,3))
 }
