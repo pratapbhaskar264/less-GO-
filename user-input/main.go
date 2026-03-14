c5ce037f71d379079587163f5577ea572c61a589
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-14 12:33:32 +0530

    DAY-4-input

--- user-input/main.go
+++ user-input/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt" 
+import ("fmt" 
+ 		"bufio" 
+ 		"os" ) 
 
 func main() {
 	// fmt.Println("hey")
-	var name string
+	// var name string
 
-	fmt.Scan(&name) // <- input taken
+	// fmt.Scan(&name) // <- input taken but only reads upto white space
+	// fmt.Println(name)
+
+	// to prevent whitespace role here we go with buffer reader 
+       
+	reader := bufio.NewReader(os.Stdin)
+	name , _ := reader.ReadString('\n')
+
 	fmt.Println(name)
+
+
+
 }
