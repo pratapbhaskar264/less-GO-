e9f94f7590ede2de18b9abe938cae82fcde2d7b0
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-15 22:55:12 +0530

    DAY-6-errorHandling

--- error_handling/main.go
+++ error_handling/main.go
@@ -15,11 +15,19 @@ func main() {
 
 	fmt.Println("Function check")
 	 
-	ans , err := divide( 10 , 0 ) 
-	if err == nil {
-		fmt.Println(ans)
-	} else{
-		fmt.Println(err)
-	}
+	// ans , err := divide( 10 , 0 ) 
+	// if err == nil {
+	// 	fmt.Println(ans)
+	// } else{
+	// 	fmt.Println(err)
+	// }
+
+	//avoid all this and use _ evry where you need to ignore the usecase -> 
+	// of something being returned
+    
+	ans , _ := divide( 10 , 4 ) 
+	
+	fmt.Println(ans)
+
 
 }
