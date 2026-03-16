1abed369e34960d63eda2be3ec191e7cc7cb0b33
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-16 15:30:46 +0530

    DAY-7-arrays

--- arrays/main.go
+++ arrays/main.go
@@ -9,13 +9,24 @@ func main() {
 	var names[5] string
 
 	names[0] = "hey"
-	names[2] = "byee"
+	names[4] = "byee"
 
-	fmt.Println( names )
+	fmt.Printf( "%q\n" , names )
     
 	var arr = [5]int{0,1,2,3,4}
 
 	fmt.Println(arr)
+	
+	fmt.Println(len(arr)) // length of array 
 
+	//default value 
+    var price[5] int
+    var val[5] string  // use printf for quoted string
+    var boolean[5] bool
+
+	fmt.Printf("empty quoted val = %q\n" , val  )
+	fmt.Println(price)
+	fmt.Println(val)
+	fmt.Println(boolean)
 
 }
