4cb715eb43b0073d96f0accbc986bab786912170
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-16 15:15:16 +0530

    DAY-7-arrays

--- arrays/main.go
+++ arrays/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("function check")
+	// fmt.Println("function check")
+    
+	//method1 , 0-based indexing
+	var names[5] string
+
+	names[0] = "hey"
+	names[2] = "byee"
+
+	fmt.Println( names )
+    
+	var arr = [5]int{0,1,2,3,4}
+
+	fmt.Println(arr)
+
+
 }
