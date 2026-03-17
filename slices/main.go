8f7697e86d4b718b0c4dd34d8ef60fb1d0f036d7
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-17 22:17:26 +0530

    DAY-8-slices

--- slices/main.go
+++ slices/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt" 
 
 func main() {
-	fmt.Println("fn check")
+	// fmt.Println("fn check")
+
+   	numbers := []int{1,2,3,4,5}
+    
+	// habbit One
+	fmt.Println("numbers is :",numbers)
+	fmt.Printf( "Type of numbers is : %T\n" , numbers )
+    
+
 }
