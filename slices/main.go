4b64a3f40060d19f7883bec0b62dd72e15f706cd
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-17 22:44:59 +0530

    DAY-8-slices

--- slices/main.go
+++ slices/main.go
@@ -13,5 +13,17 @@ func main() {
     fmt.Println(len(numbers))
 	numbers = append(numbers ,3,4,4) //append works this way
 	fmt.Println(numbers)
+    
+	names := []string{};
+	names = append(names , "kunal" , "bhaskar" ,"rohitsharma" , "aditya")
+	fmt.Println(names)
+
+	//habbit 2nd where we use make function to set length and capacity 
+    
+	list := make( []int , 2 , 5 )
+	list = append(list , 1,2,3,5,4,4,4,4,4,4,4)
+	fmt.Println(list)
+	fmt.Println(len(list))
+	fmt.Println(cap(list))
 
 }
