8243ba8192b2475c932a69a12dfd8bae322e7314
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-18 23:17:26 +0530

    DAY-10-forLoop

--- for-Loop/main.go
+++ for-Loop/main.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("function check")
 
-	var i int = 0
+	// var i int = 0
 
-	for   ; i < 10 ; i++  { // intitalization 
-		fmt.Println(i)
+	// for  ; i < 10 ; i++  { // intitalization 
+	// 	fmt.Println(i)
+	// }
+
+	number := []int{1,2,3,4}
+    fmt.Println(number)
+	for index , value := range number{
+		fmt.Printf(" %d %d \n" , index , value)
 	}
 }
