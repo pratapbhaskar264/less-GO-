8ec1d253356eb906c1f4bb208c950e60588f6431
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-18 22:22:55 +0530

    DAY-9-switchCase

--- switch-case/main.go
+++ switch-case/main.go
@@ -1,7 +1,25 @@
 package main 
  
-import "fmt"
+import ("fmt"
+         "bufio"
+		 "os"
+		  "strings"
+		"strconv")
 
 func main(){
 	fmt.Println("hey")
+   
+	//user input
+	reader := bufio.NewReader(os.Stdin)
+	input , _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	value , _ := strconv.Atoi(input)
+	
+	//switch case
+	switch value {
+	case 1:
+		fmt.Println("one")
+	default :
+		fmt.Println("Htt jaa tu")
+	}
 }
