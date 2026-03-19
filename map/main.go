d0dd10ecc61ad48e16a197bf09b56d450069f2f0
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-19 23:11:59 +0530

    DAY-9-maps

--- map/main.go
+++ map/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+// key value pair of unordered data 
+
 func main(){
-	fmt.Println("function check")
+	// fmt.Println("function check")
+	student := make(map[string]int)
+
+	student["bhaskar"] = 99
+	student["chomu"] = 95
+	student["kunal"] = 95
+
+	fmt.Println(student)
+	delete(student , "chomu")
+	value , existance := student["chomu"]
+	if !existance  {
+		fmt.Println("chomu not found")
+	} else{
+		fmt.Println(value)
+	}
+	fmt.Println(student["kunal"])
+	fmt.Println(student)
 }
