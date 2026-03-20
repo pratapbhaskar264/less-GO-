5949c907c1b5afa4d04c841a037b92f3c26f8a73
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-20 23:55:26 +0530

    DAY-12-structs

--- struct/main.go
+++ struct/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type Student struct {
+type student struct {
 	name string
 	marks int
 }
@@ -10,8 +10,21 @@ type Student struct {
 func main() {
   fmt.Println("Structs in Go")
   
-  var student1 Student
+  var student1 student
   student1.name = "bhaskar"
   student1.marks = 99
   fmt.Println(student1)
+
+  student2 := student {
+	  name : "kunal",
+	  marks : 99,
+	}
+
+  fmt.Println(student2)
+
+ // 3rd method
+//   var person2 = new(Student)
+//   person2. .....
+
+// we can create an struct of structs as well
 }
