f23b626cdde8586a9259e458b5c19a10f481378a
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-20 23:39:32 +0530

    DAY-12-structs

--- struct/main.go
+++ struct/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type Student struct {
+	name string
+	marks int
+}
+
 func main() {
   fmt.Println("Structs in Go")
+  
+  var student1 Student
+  student1.name = "bhaskar"
+  student1.marks = 99
+  fmt.Println(student1)
 }
