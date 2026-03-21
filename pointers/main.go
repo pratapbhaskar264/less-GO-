091c8b8ccde10e64ec55d14d19bef86ec7b660fc
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-21 20:28:43 +0530

    DAY-13-pointers

--- pointers/main.go
+++ pointers/main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+func add(a , b *int) int {
+	return (*a)+(*b);
+}
+
 func main() {
 	fmt.Println("Pointers in Go")
+
+	var num1 int = 36
+
+	var num2 *int = &num1
+
+	fmt.Println(num1)
+	fmt.Println("value at address of nums1 :" , *num2)
+	fmt.Println("address of num2 :",&num2)
+	fmt.Println("address of num1 :",num2)
+
+	val := 45 
+	ptr := &val
+
+	fmt.Println(val)
+	fmt.Println(*ptr)
+    
+	fmt.Println(add(&val , ptr ))
+
+	a := 6
+
+	b := 3
+
+    fmt.Println(add(&a , &b))
 }
