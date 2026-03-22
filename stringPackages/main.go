0491c12c5a71ed3a48036bc7875d771640cd631d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-22 15:30:56 +0530

    DAY-15-stringPackages

--- stringPackages/main.go
+++ stringPackages/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	data := "  hey,how,are,you,guys,doing,hey  "
+
+	parts := strings.Split(data, ",")
+
+	fmt.Println(parts)
+	fmt.Println(strings.Count(data, "hey"))
+
+	trim := strings.Trim(data, " ")
+
+	fmt.Println(trim)
+
+	str1 := "Bhaskar"
+
+	str2 := "Pratap"
+
+	con := strings.Join([]string{str1, str2}, " ")
+
+	fmt.Println(con)
+
 }
