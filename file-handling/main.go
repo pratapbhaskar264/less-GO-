e663eb6db033483c7e157ad19ae3bf1c37cbe19f
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-23 23:17:33 +0530

    DAY-17-FileHandling

--- file-handling/main.go
+++ file-handling/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("heyhey")
+
+	file, err := os.Create("exe.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("file created")
+
+	content := "hello world"
+
+	file.Write([]byte(content))
+	file.Write([]byte(" heyhey"))
+	// file.Read([]byte , 10)
 }
