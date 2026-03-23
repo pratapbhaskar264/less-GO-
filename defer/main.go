d3f79c3662fb4793ba874c5b152c08c1609b1f99
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-23 23:04:03 +0530

    DAY-16-deferKeyword

--- defer/main.go
+++ defer/main.go
@@ -11,4 +11,7 @@ func main() {
 	//more than one defer are excecuted in stack order .... lifo
 	defer fmt.Println(add(2, 3))
 	fmt.Println("hello")
+
+	// defer will be used say file opend , defer and close it in very next line , it'll will be closed at the end of function (reduces chance of failure if we forget to close it later)
+
 }
