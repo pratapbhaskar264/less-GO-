f084004505c0bb4a415a06fca8aa74b18af06b24
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-23 23:26:49 +0530

    DAY-17-FileHandling

--- file-handling/main.go
+++ file-handling/main.go
@@ -2,24 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fmt.Println("heyhey")
 
-	file, err := os.Create("exe.txt")
+	// file, err := os.Create("exe.txt")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer file.Close()
+	// fmt.Println("file created")
+
+	// content := "hello world"
+
+	// file.Write([]byte(content))
+	// file.Write([]byte(" heyhey"))
+	// file.Read([]byte , 10)
+
+	//READ
+
+	file, err := os.Open("exe.txt")
+
+	buffer := make([]byte, 1024)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer file.Close()
-	fmt.Println("file created")
 
-	content := "hello world"
+	for {
+		f, error := file.Read(buffer)
 
-	file.Write([]byte(content))
-	file.Write([]byte(" heyhey"))
-	// file.Read([]byte , 10)
+		if error == io.EOF {
+			break
+		}
+
+		if error != nil {
+			fmt.Println(error)
+			return
+		}
+
+		fmt.Println(string(buffer[:f]))
+
+	}
+
 }
