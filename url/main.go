a64e07fe5cc52a3c8cb617750ded026dc6ca6ee7
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-24 23:02:09 +0530

    DAY-19-URL

--- url/main.go
+++ url/main.go
@@ -2,12 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	// "io"
 	// "net/http"
 )
 
 func main() {
+	fmt.Println("URL FILE : ")
 
-	fmt.Println("URL")
+	myURL := "https://www.youtube.com/watch?v=vu6ZQ-t1sUk&list=PLzjZaW71kMwSEVpdbHPr0nPo5zdzbDulm&index=26"
+
+	parsedURL, err := url.Parse(myURL)
+
+	if err != nil {
+		fmt.Println("cannot be parsed")
+	}
+
+	fmt.Println(parsedURL.Host)
+	fmt.Println(parsedURL.Scheme)
+	fmt.Println(parsedURL.Path)
+	fmt.Println(parsedURL.RawQuery)
+
+	parsedURL.Host = "lalalala" // modifying
+
+	newURl := parsedURL.String() // creating url
+
+	parsedURL2, err := url.Parse(newURl)
+
+	if err != nil {
+		fmt.Println("cannot be parsed")
+	}
+
+	fmt.Println(parsedURL2.Host)
+	fmt.Println(parsedURL2.Scheme)
+	fmt.Println(parsedURL2.Path)
+	fmt.Println(parsedURL2.RawQuery)
 
 }
