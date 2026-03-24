f36dcea87e2256cdc3789bc4d6d5f52444eabcbf
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-24 22:40:59 +0530

    DAY-18-webRequest

--- webRequest/main.go
+++ webRequest/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+	if err != nil {
+		fmt.Println("Error getting data")
+		return
+	}
+
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("Error getting data")
+		return
+	}
+
+	fmt.Println(string(data))
+
 }
