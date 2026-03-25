0df244d11a00c0c8b93ec396110b1b1dbc84a346
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-25 23:36:58 +0530

    DAY-20-Json

--- Json/main.go
+++ Json/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 func main() {
-	fmt.Println("JSON FILE : ")
+	fmt.Println("JSON FILE")
+
+	person1 := Person{Name: "Bhaskar", Age: 22}
+
+	fmt.Println(person1)
+	fmt.Println(person1.Age)
+
+	// person1 to Json encoding (marshalling)
+
+	jsonData, err := json.Marshal(person1)
+
+	if err != nil {
+		fmt.Println("Cannot be marshelled")
+		return
+	}
+
+	fmt.Println(string(jsonData))
+
+	// decoding unmarshelling
+
+	var person11 Person
+
+	err = json.Unmarshal(jsonData, &person11)
+
+	if err != nil {
+		fmt.Println("not able to demarshal")
+		return
+	}
+
+	fmt.Println(person11)
+
 }
