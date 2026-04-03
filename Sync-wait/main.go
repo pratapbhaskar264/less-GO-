1425bf13e7f84d3b89369cc8666c22a5691cf0e0
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-04-03 08:43:47 +0530

    DAY-25-GoRoutines

--- Sync-wait/main.go
+++ Sync-wait/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(i int) {
+	defer wq.Done()
+	fmt.Printf("worker %d starting\n", i)
+	fmt.Printf("worker %d ending\n", i)
+}
 
 func main() {
 	fmt.Println("heyhey")
+
+	var wq sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wq.Add(1)
+		go worker(i)
+	}
 }
