f1bc0d5a17a62f1fe932cc580d4fd7d517e8bf37
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-04-06 22:02:55 +0530

    DAY-25-GoRoutines

--- Sync-wait/main.go
+++ Sync-wait/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func worker(i int, wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // tells counter to increment by 1 when the function is done
 	fmt.Printf("worker %d starting\n", i)
 	fmt.Printf("worker %d ending\n", i)
 }
@@ -14,11 +14,11 @@ func worker(i int, wg *sync.WaitGroup) {
 func main() {
 	fmt.Println("heyhey")
 
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // check list or task list
 
 	for i := 1; i <= 5; i++ {
-		wg.Add(1)
+		wg.Add(1) // increment the WaitGroup counter
 		go worker(i, &wg)
 	}
-	wg.Wait()
+	wg.Wait() // wait for all goroutines to finish
 }
