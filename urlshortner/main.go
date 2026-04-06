8462ed1ebd3c7b6a1bcc71cf75c7a5258baead3b
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-04-06 22:09:50 +0530

    structCreation

--- urlshortner/main.go
+++ urlshortner/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Url shortener")
 }
